20f112e1d0b14ea04144e69f13df0474628f12ac
Xinran <zhxyn6@gmail.com>
2023-01-07 21:15:45 -0500

    A magic ball to tell you yes or no
    using random function, array.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
 
-
+    @IBAction func askButtonPresses(_ sender: UIButton) {
+        let ballArray = [UIImage(imageLiteralResourceName: "ball1"),
+                         UIImage(imageLiteralResourceName: "ball2"),
+                         UIImage(imageLiteralResourceName: "ball3"),
+                         UIImage(imageLiteralResourceName: "ball4"),
+                         UIImage(imageLiteralResourceName: "ball5")
+        ]
+        
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 
 }
 
